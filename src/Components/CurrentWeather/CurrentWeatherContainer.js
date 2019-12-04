2f2d3389ff53dc86820bce398151bd6b8895a91b
Aleece <aleeceburgess@gmail.com>
2019-12-04 12:19:56 +0800

    Cheeky little weather widget header

---
+++ src/Components/CurrentWeather/CurrentWeatherContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class CurrentWeatherContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  
+
+  
+
+  render = () => { 
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default CurrentWeatherContainer;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import WeatherCardContainer from "./Components/WeatherCard/WeatherCardContainer";
+import UpcomingWeatherCardContainer from "./Components/UpcomingWeatherCard/UpcomingWeatherCardContainer";
+import CurrentWeather from "./Components/CurrentWeather/CurrentWeatherContainer";
+import Header from "./Components/Header/Header";
 
 import "./styles/main.css";
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      location: 'London',
+      locationDate: 0
+    }
+  }
+
+  componentDidMount = () => {
+    this.getLocalDate();
+  }
+
+  getLocalDate = () => {
+    let offsetHours = 0;
+    switch (this.state.location) {
+      case 'London':
+        offsetHours = 0;
+        break;
+      case 'Prague':
+        offsetHours = +1;
+        break;
+      case 'Perth':
+        offsetHours = +8;
+        break;
+      case 'Tokyo':
+        offsetHours = +9;
+        break;
+      case 'San Fransico':
+        offsetHours = -8;
+        break;
+      default:
+        offsetHours = 0
+    }
+    this.setState({
+      locationDate: new Date( new Date() + offsetHours * 3600 * 1000).toDateString()
+    })
+  }
+
+  updateLocationClick = (newLocation) => {
+    this.getLocalDate();
+    this.setState({
+      location: newLocation
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="weather-cards">
-          <WeatherCardContainer />
+        <Header
+          updateLocationClick={this.updateLocationClick} />
+        <div className="weather-widget">
+          <div className="widget-header">
+            <h2>Weather report</h2>
+            <p className="location">{this.state.locationDate} | {this.state.location}</p>
+          </div>
+          <div className="current-weather">
+            <CurrentWeather />
+          </div>
+          <div className="upcoming-weather-cards">
+            <UpcomingWeatherCardContainer />
+          </div>
         </div>
       </div>
     );
