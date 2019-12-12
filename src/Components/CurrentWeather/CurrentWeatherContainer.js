80a5f039279319ffa6f54cb0be09ca7ce3faf051
Aleece <aleeceburgess@gmail.com>
2019-12-12 13:58:23 +0800

    Trailing Lottie for the first time

--- src/Components/CurrentWeather/CurrentWeatherContainer.js
+++ src/Components/CurrentWeather/CurrentWeatherContainer.js
@@ -1,20 +1,32 @@
 import React from "react";
+import Lottie from 'react-lottie'
+import animationData from './lotties/4801-weather-partly-shower.json'
 
 class CurrentWeatherContainer extends React.Component {
   constructor(props) {
     super(props);
-
-
   }
-
   
 
-  
+  render(){
 
-  render = () => { 
-    return (
-      <h1>Hello</h1>
-    );
+    const defaultOptions = {
+      loop: true,
+      autoplay: true, 
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return(
+      <div>
+        <Lottie options={defaultOptions}
+              height={100}
+              width={100}
+        />
+      </div>
+    )
   }
 }
 
